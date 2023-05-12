5d04029b5c9512ab3d11316e078f935f0ada6789
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-05-12 19:55:56 +0600

    Auth Provider and firbase key added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
 import loginLogo from '../../assets/images/login/login.svg'
 
-const Login = () => {
-  return (
-    <div className="hero min-h-screen bg-base-200">
+const SignUp = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row ">
         <div className="text-center lg:text-left w-1/2 md:mr-[120px] ">
           
@@ -11,7 +11,17 @@ const Login = () => {
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
-          <h1 className="text-2xl text-center font-bold">Login</h1>
+          <h1 className="text-2xl text-center font-bold">Sign Up</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -38,14 +48,14 @@ const Login = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <input className="btn btn-primary" type="submit" value="Sign Up" />
             </div>
           </div>
-          <p className='mx-auto my-4 text-sm'>New to Car Doctor? <Link className='text-red-500' to="/signup">Sign Up</Link></p>
+          <p className='mx-auto my-4 text-sm'>Already Have An Account ? <Link className='text-red-500' to="/login">Login</Link></p>
         </div>
       </div>
     </div>
-  );
+    );
 };
 
-export default Login;
+export default SignUp;
